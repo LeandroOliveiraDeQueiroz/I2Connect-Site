b0ddcf2c05692392b8415831991fff07849d33be
Leandro Oliveira de Queiroz <leandroliveiradequeiroz@gmail.com>
2019-08-31 13:13:21 -0300

    Testing php mail

---
+++ js/send-email.js
@@ -0,0 +1,12 @@
+const enviar = document.getElementById("send-form");
+
+enviar.addEventListener("click",()=>{
+
+    const nome = document.getElementById("name");
+    const email = document.getElementById("email");
+    const numero = document.getElementById("number");
+    const mensagem = document.getElementById("message");
+
+    var file = new Blob([php]);
+
+});
